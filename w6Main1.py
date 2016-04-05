1ad7ec21a838f9704bf12b4484b9f19d9bb42849
201611099 <yan04121@naver.com>
2016-04-05 10:51:29 +0900

    w6.py
    ggg

---
+++ w6Main1.py
@@ -0,0 +1,9 @@
+﻿bmi=22
+if bmi < 18.5:
+    print "under weight"
+elif bmi >= 18.5 and bmi < 23:
+    print "normal weight"
+elif bmi >= 23 and bmi <= 25:
+    print "over weight"
+elif bmi > 25:
+    print "obesity"
