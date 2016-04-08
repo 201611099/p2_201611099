0839fc2b6cf84e1c74fda11c3fb4ce797be2f8f0
201611099 <yan04121@naver.com>
2016-04-08 09:45:02 +0900

    Leap year

---
+++ w6Main5.py
@@ -0,0 +1,22 @@
+﻿"""
+@author LHJ
+@since 160406
+"""
+def Leapyear():
+    year=raw_input("year: ")
+    year=int(year)
+    if (year%4==0) and (year%100!=0 or year%400==0):
+        res="It's a Leap year"
+    else:
+        res="It isn't a Leap year"
+    print res
+    return res
+
+def lab6()
+    Leapyear()
+
+def main():
+    lab6()
+
+if __name__=="__main__":
+    main()
