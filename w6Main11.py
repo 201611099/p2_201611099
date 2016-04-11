6f61f1d9db58f4f8af8d96d0657c0af7574d4490
201611099 <yan04121@naver.com>
2016-04-11 22:11:46 +0900

    2016.4.11
    4의 배수O 5의 배수X

---
+++ w6Main11.py
@@ -0,0 +1,24 @@
+﻿"""
+201611099
+"""
+def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if i%4==0 and i%5!=0:
+            x.append(i)
+    print x
+    sum=0
+    for i in range(0,len(x)):
+        if i%4==0 and i%5!=0:
+            sum+=x[i]
+    print sum
+
+def lab6(ms):
+    aList=1000
+    mysum=sumList(aList)  
+    print mysum
+ms=1000
+lab6(ms)
+
+def main():
+    lab6()
