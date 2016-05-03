427499ebb500e6b8ddec6c4fb6d7e1d8cd529e21
201611099 <yan04121@naver.com>
2016-05-03 20:11:32 +0900

    2016.5.2
    데이터구조-5

---
+++ w9Main(5).py
@@ -0,0 +1,24 @@
+﻿def charCount(word):
+    d=dict()
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+    print d
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)),list(d.keys()))
+    plt.show()
+
+word='sangmyung university'
+def lab9():
+    charCount(word)
+    
+def main():
+    lab9()
+    
+if __name__=="__main__":
+    main()
