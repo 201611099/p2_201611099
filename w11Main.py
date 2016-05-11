37c28d295cfcbf8205ca0e91f8415e44f2193553
201611099 <yan04121@naver.com>
2016-05-12 00:22:20 +0900

    2016.5.11
    event(선)

---
+++ w11Main.py
@@ -0,0 +1,44 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+t2=turtle.Turtle()
+t2.penup()
+t2.setpos(100,200)
+t2.pendown()
+t2.forward(200)
+
+def keyup():
+         t1.forward(50)
+
+def keydown():
+         t1.back(50)
+
+
+def keyleft():
+         t1.left(90)
+
+def keyright():
+         t1.right(90)
+
+def mousegoto(x,y):
+     t1.setpos(x,y)
+     t1.pos=(x,y)
+     if 100<=x<=300 and 200<=y<=200:
+          print "Correct"
+def addKeys():
+     wn.onkey(keyup,"Up")
+     wn.onkey(keydown,"Down")
+     wn.onkey(keyleft,"Left")
+     wn.onkey(keyright,"Right")
+
+def addMouse():
+     wn.onclick(mousegoto)
+
+addKeys()
+addMouse()
+
+wn.listen()
+turtle.mainloop()
+
+
