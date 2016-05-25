fbfbc954124ba3253319e8369ef737f0070cf651
201611099 <yan04121@naver.com>
2016-05-25 20:48:31 +0900

    2016.5.25
    ??

---
+++ w13Main.py
@@ -0,0 +1,15 @@
+﻿def outputNumber():
+    data=[1,2,3,4,5,6,7,8,9,10]
+    for i in data:
+        print "{0}\t".format(i),
+        if i%2==0:
+            print
+            
+def lab13():
+    outputNumber()
+    
+def main():
+    lab13()
+
+if __name__=="__main__":
+    main() 
